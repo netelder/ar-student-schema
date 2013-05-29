d0df427b4117cbd46697e37225c9c9ac8154fadf
Apprentice <apprentice@dbc25.local>
2013-05-29 15:58:52 -0700

    migrate and student.db initial checkin

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,17 @@
 require_relative '../../db/config'
 
+class Student < ActiveRecord::Base
+  validates :email, :birthday => true
+
+  def name
+    self.first_name + " " + self.last_name
+  end
+
+  def age
+    ((DateTime.now - self.birthday)/365).to_i
+  end
+
+end
+
 # implement your Student model here
+# email validation regex: \S+@\S+.\S{2,}
--- db/migrate/20121011144238_create_students.rb
+++ db/migrate/20121011144238_create_students.rb
@@ -3,7 +3,15 @@ require_relative '../config'
 # this is where you should use an ActiveRecord migration to 
 
 class CreateStudents < ActiveRecord::Migration
+  # HINT: checkout ActiveRecord::Migration.create_table
   def change
-    # HINT: checkout ActiveRecord::Migration.create_table
+    create_table(:students) do |t|
+      t.string   :first_name
+      t.string   :last_name
+      t.string   :gender
+      t.date     :birthday
+      t.string   :email
+      t.string   :phone
+    end
   end
 end
